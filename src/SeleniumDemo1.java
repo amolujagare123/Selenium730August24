132777f941b1777b33ef3f1c6860e94b7b83d2c1
amolu <amolujagare@gmail.com>
2024-08-17 17:02:44 +0530

    Initial commit

---
+++ src/SeleniumDemo1.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class SeleniumDemo1 {
+
+    public static void main(String[] args) {
+
+     //   WebDriver driver = new ChromeDriver(); // upcasting
+
+      //  WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new EdgeDriver();
+
+      //  WebDriver driver = new SafariDriver();
+
+     //  WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://facebook.com");
+
+    }
+}
