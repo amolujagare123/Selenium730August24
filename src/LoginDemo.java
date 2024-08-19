007b1c713b080ea1001c4f40620d9e72e0f64678
amolu <amolujagare@gmail.com>
2024-08-19 23:30:10 +0530

    selenium

---
+++ src/LoginDemo.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver(); // upcasting
+
+        driver.manage().window().maximize();
+        driver.get("https://stock.scriptinglogic.in/");
+
+        WebElement txtUsername = driver.findElement(By.id("login-username"));
+        txtUsername.sendKeys("admin");
+
+        WebElement txtPassword = driver.findElement(By.id("login-password"));
+        txtPassword.sendKeys("admin");
+
+        WebElement btnLogin = driver.findElement(By.name("submit"));
+        btnLogin.click();
+
+        //WebElement lnkAddCustomer = driver.findElement(By.linkText("Add Customer"));
+        WebElement lnkAddCustomer = driver.findElement(By.partialLinkText("d Cust"));
+        lnkAddCustomer.click();
+
+        driver.findElement(By.name("name")).sendKeys("rajeshwari");
+        driver.findElement(By.name("address")).sendKeys("xyz");
+        driver.findElement(By.name("contact1")).sendKeys("656565656");
+        driver.findElement(By.name("contact2")).sendKeys("65656565");
+        driver.findElement(By.name("Submit")).click();
+
+
+
+
+
+
+
+    }
+}
--- src/SeleniumDemo1.java
+++ src/SeleniumDemo1.java
@@ -8,16 +8,20 @@ public class SeleniumDemo1 {
 
     public static void main(String[] args) {
 
-     //   WebDriver driver = new ChromeDriver(); // upcasting
+       WebDriver driver = new ChromeDriver(); // upcasting
 
       //  WebDriver driver = new FirefoxDriver();
-        WebDriver driver = new EdgeDriver();
+     //   WebDriver driver = new EdgeDriver();
 
       //  WebDriver driver = new SafariDriver();
 
      //  WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://facebook.com");
+        driver.get("http://gmail.com");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
 
     }
 }
