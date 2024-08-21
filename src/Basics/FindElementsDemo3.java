4dd888fcc3343f2141ba3d4f46255603a358f0f1
amolu <amolujagare@gmail.com>
2024-08-21 20:40:52 +0530

    selenium

--- src/Basics/FindElementsDemo3.java
+++ src/Basics/FindElementsDemo3.java
@@ -1,3 +1,5 @@
+package Basics;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -5,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
-public class FindElementsDemo {
+public class FindElementsDemo3 {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
@@ -13,14 +15,15 @@ public class FindElementsDemo {
         driver.manage().window().maximize();
         driver.get("https://stock.scriptinglogic.in/");
 
-        List<WebElement> wbList = driver.findElements(By.tagName("input"));
+        List<WebElement> wbList = driver.findElements(By.className("round"));
 
-        System.out.println(wbList.size());
+       System.out.println(wbList.size()); // 4
 
-        wbList.get(0).sendKeys("admin");
-        wbList.get(1).sendKeys("admin");
-        wbList.get(2).click();
+        wbList.get(0).sendKeys("admin"); // username
+        wbList.get(1).sendKeys("admin"); // password
+        //wbList.get(2).click(); // button
 
+        System.out.println(wbList.get(3).getText());
 
     }
 }
---
+++ src/xpathDemo/LoginDemo.java
@@ -0,0 +1,45 @@
+package xpathDemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver(); // upcasting
+
+        driver.manage().window().maximize();
+        driver.get("https://stock.scriptinglogic.in/");
+
+        WebElement txtUsername = driver.findElement(By.xpath("//input[@id='login-username']"));
+        txtUsername.sendKeys("admin");
+
+        WebElement txtPassword = driver.findElement(By.xpath("//input[@name='password']"));
+        txtPassword.sendKeys("admin");
+
+        WebElement btnLogin = driver.findElement(By.xpath("//input[@type='submit']"));
+        btnLogin.click();
+/*
+
+        //WebElement lnkAddCustomer = driver.findElement(By.linkText("Add Customer"));
+        WebElement lnkAddCustomer = driver.findElement(By.partialLinkText("d Cust"));
+        lnkAddCustomer.click();
+
+        driver.findElement(By.name("name")).sendKeys("rajeshwari");
+        driver.findElement(By.name("address")).sendKeys("xyz");
+        driver.findElement(By.name("contact1")).sendKeys("656565656");
+        driver.findElement(By.name("contact2")).sendKeys("65656565");
+        driver.findElement(By.name("Submit")).click();
+*/
+
+
+
+
+
+
+
+    }
+}
